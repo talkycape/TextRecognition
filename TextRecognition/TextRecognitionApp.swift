cd4379e8b55831a4dc50327cdfc899e30bdf9f64
Y. Lai <yachieh@gmail.com>
2022-12-26 09:40:15 -0800

    Initial Commit

---
+++ TextRecognition/TextRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextRecognitionApp.swift
+//  TextRecognition
+//
+//  Created by YACHIEH LAI on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TextRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
